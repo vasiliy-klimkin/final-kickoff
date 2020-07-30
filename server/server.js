79a9d17d7c98f1c6963223ece4a1c1ca83c9398d
vasiliy-klimkin <vasily.klimkin@gmail.com>
2020-07-30 11:34:01 -0400

    route added!

--- server/server.js
+++ server/server.js
@@ -2,5 +2,8 @@ const express = require('express');
 
 const server = express();
 
+server.get('/', (req, res) => {
+    res.json({foo:'bar'});
+})
 
 server.listen(8080, ()=> console.log("Server is Listening on port 8080"));
